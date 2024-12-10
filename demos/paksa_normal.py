a26d9b17003bcbeb132d870f87b80e1ed9d34270
Atyanta <atyanta.uksw@gmail.com>
2024-12-10 18:39:10 +0700

    Update paksa_normal.py

--- demos/paksa_normal.py
+++ demos/paksa_normal.py
@@ -3,6 +3,8 @@ import cv2
 import torch
 import argparse
 from tqdm import tqdm
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from decalib.deca import DECA
 from decalib.datasets import datasets 
 from decalib.utils import util
