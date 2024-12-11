1abf952866e66a6fec1fbc93f26c705f2c13db1c
Atyanta <atyanta.uksw@gmail.com>
2024-12-11 14:32:50 +0700

    Update paksa_normal.py

--- demos/paksa_normal.py
+++ demos/paksa_normal.py
@@ -24,9 +24,9 @@ def main(args):
     testdata = datasets.TestData(args.inputpath, iscrop=args.iscrop, face_detector=args.detector)
     
     # Inisialisasi DECA
-    deca_cfg.model.use_tex = args.useTex
-    deca_cfg.model.extract_tex = args.extractTex
-    deca_cfg.rasterizer_type = args.rasterizer_type
+    deca_cfg.model.use_tex = args.useTex  # Menentukan apakah menggunakan FLAME texture
+    deca_cfg.model.extract_tex = args.extractTex  # Menentukan apakah akan mengekstrak tekstur dari gambar
+    deca_cfg.rasterizer_type = args.rasterizer_type  # Jenis rasterizer
     deca = DECA(config=deca_cfg, device=device)
 
     visdict_list_list = []
@@ -76,18 +76,30 @@ if __name__ == '__main__':
                         help='path to the output directory, where results(obj, txt files) will be stored.')
     parser.add_argument('--device', default='cuda', type=str,
                         help='set device, cpu for using cpu')
+
+    # Argument extractTex untuk mengekstrak tekstur atau albedo map
     parser.add_argument('--extractTex', default=True, type=lambda x: x.lower() in ['true', '1'],
                     help='whether to extract texture from input image as the uv texture map, '
                          'set false if you want albedo map from FLAME model')
+
+    # Argument useTex untuk menggunakan model FLAME untuk tekstur
     parser.add_argument('--useTex', default=False, type=lambda x: x.lower() in ['true', '1'],
-                        help='whether to use FLAME texture model to generate uv texture map, \
-                            set it to True only if you downloaded texture model' )    
+                        help='whether to use FLAME texture model to generate uv texture map, '
+                             'set it to True only if you downloaded texture model')    
+
+    # Argument rasterizer_type untuk jenis rasterizer
     parser.add_argument('--rasterizer_type', default='standard', type=str,
                         help='rasterizer type: pytorch3d or standard')
+
+    # Argument iscrop untuk memilih apakah gambar akan dipotong (crop)
     parser.add_argument('--iscrop', default=True, type=lambda x: x.lower() in ['true', '1'],
                         help='whether to crop input image, set false only when the test image are well cropped')
+
+    # Argument detector untuk memilih detektor wajah
     parser.add_argument('--detector', default='fan', type=str,
                         help='detector for cropping face, check detectos.py for details')
+
+    # Argument saveObj untuk menyimpan file .obj hasil rekonstruksi
     parser.add_argument('--saveObj', action='store_true', 
                         help='whether to save the output as .obj files')
 
